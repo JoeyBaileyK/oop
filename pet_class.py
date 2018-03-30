d2be486ad43e2036b70091ffe4d646ec1ea9b09d
Joey <joeybailey284@gmail.com>
2018-03-30 11:44:51 -0400

    add pet class

---
+++ pet_class.py
@@ -0,0 +1,26 @@
+#pet class
+class pet:
+    foodLevel = 100
+    def __init__(self, name):
+        self.name = name
+
+    def __init__(self, weight):
+        self.weight = weight
+
+
+s = ('steve')
+
+b = ('bob')
+
+k = ('kevin')
+
+c = ('carl')
+
+s.add_weight(10)
+b.sub_weight(10)
+
+print(s.name)
+print(s.weight)
+
+print(s.name)
+print(s.weight)
